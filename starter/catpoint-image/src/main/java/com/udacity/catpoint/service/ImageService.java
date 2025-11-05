bc94228b24beb449ddbcaf66113e7ddb7d64d5f1
Shivam Singh <adityashivamsingh03@gmail.com>
2025-11-05 23:15:37 +0530

    changes

---
+++ starter/catpoint-image/src/main/java/com/udacity/catpoint/service/ImageService.java
@@ -0,0 +1,6 @@
+package com.udacity.catpoint.image.internal;
+
+// Placeholder moved: original ImageService moved to com.udacity.catpoint.image.service.ImageService
+// Kept a non-public placeholder to avoid split-package issues during migration.
+class _ImageServicePlaceholder {
+}
